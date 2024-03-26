5da15c19d258ecdd7b91357bf0985f9793ad61d3
Nhlanhla Mbokazi <nhlanhla.mbokazi@dadevs.co.za>
2024-03-26 17:47:21 +0200

    Removed Images

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -36,19 +36,21 @@ const Projects = () => {
       <h2 className="text-gray-800 text-4xl font-bold text-center mb-8">Gallery</h2>
 
       <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8 shadow-lg border-solid border-gray-800">
-        {images.map((image) => (
+        {/* {images.map((item, index) => (
        
             <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
               <Image
-                key={image.id}
-                src={image.image}
-                quality={100}
+                key={item.id ?? index}
+                src={item.image}
+                
                 alt=""
                 className="h-full w-full object-cover object-center group-hover:opacity-75"
               />
             </div>
            
-        ))}
+        ))} */}
+          Images will go here
+
       </div>
     </div>
   </div>
