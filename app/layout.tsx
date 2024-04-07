fc6cab5cff6fd7a5302679bfddba92a4999357bc
Nhlanhla Mbokazi <nhlanhla.mbokazi@dadevs.co.za>
2024-04-07 13:15:38 +0200

    speed insight added

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,8 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "./header/page";
 import Footer from "./footer/page";
-import { Analytics } from "@vercel/analytics/react"
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -24,6 +25,7 @@ export default function RootLayout({
         <Header /> 
         {children}
         <Analytics />
+        <SpeedInsights />
         <Footer />
       </body>
     </html>
